8636e052283c460a7c50fe1d80c211dd4037980a
q417996520 <417996520@qq.com>
2018-09-26 23:13:53 +0800

    COMMIT_EDITMSG [unix] (23:13 26/09/2018)

---
+++ wrzy/app.js
@@ -0,0 +1,11 @@
+//app.js
+App({
+  onGotUserInfo: function (e) {
+    console.log(e.detail.errMsg)
+    console.log(e.detail.userInfo)
+    console.log(e.detail.rawData)
+  },
+  globalData: {
+    userInfo: null
+  }
+})
